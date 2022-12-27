01342e6ca728ac1c1545af0f87d4ac326c41ee2b
Alexander Podstrechnyy <tankalxat34@gmail.com>
2022-12-27 22:09:57 +0300

    Added tags "meta viewport"

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const USER_TABLE_VALUE = 1
 const ENEMY_TABLE_VALUE = 2
 
 
+var counterMoves = 0
+
 var table = [
     [null, null, null],
     [null, null, null],
@@ -12,30 +14,19 @@ var table = [
 ]
 
 
-function getIndexes(value_in_table = USER_TABLE_VALUE) {
-    /* 
-    Return all indexes where USER_SYMBOL or ENEMY_SYMBOL is standing.
-
-    */
-    let result = new Array()
-    for (let i = 0; i < 9; i++) {
-        if (table[i] == value_in_table) result.push(i)
-    }
-    return result
-}
-
 function showWinner(value_in_table = USER_TABLE_VALUE) {
     if (value_in_table === USER_TABLE_VALUE) {
-        document.getElementById("inf-text").innerText = "You winned!"
+        document.getElementById("inf-text").innerHTML = "<b>X</b> is winned!"
         document.getElementById("inf").classList.add("win")
-    } else {
-        document.getElementById("inf-text").innerText = "You losed!"
-        document.getElementById("inf").classList.add("lose")
-    }        
+    } else if (value_in_table === ENEMY_TABLE_VALUE) {
+        document.getElementById("inf-text").innerHTML = "<b>0</b> is winned!"
+        document.getElementById("inf").classList.add("win")
+    }
+    document.getElementById("btn-clear").innerText = "Play again!"
 }
 
 
-function makeMove(row, column, symbol) {
+function makeMove(row, column, symbol = null) {
     /* 
     Make a move 
     */
@@ -43,14 +34,24 @@ function makeMove(row, column, symbol) {
         value_in_table = USER_TABLE_VALUE
     } else if (symbol == ENEMY_SYMBOL) {
         value_in_table = ENEMY_TABLE_VALUE
+    } else if (symbol === null) {
+        if (counterMoves % 2 === 0) {
+            value_in_table = USER_TABLE_VALUE
+            symbol = USER_SYMBOL
+        } else {
+            value_in_table = ENEMY_TABLE_VALUE
+            symbol = ENEMY_SYMBOL
+        }
     }
 
     if (table[row][column] === null && table[row][column] !== USER_TABLE_VALUE && table[row][column] !== ENEMY_TABLE_VALUE) {
         document.getElementById(`cell-${row}-${column}`).innerText = symbol
         table[row][column] = value_in_table
         checkToWin(value_in_table)
+        counterMoves++
     }
 }
+
 
 function TransMatrix(A)
 {
@@ -63,13 +64,15 @@ function TransMatrix(A)
     return AT;
 }
 
+
 function checkToWin(value_in_table = USER_TABLE_VALUE) {
     /*
     Return the player value in table that is winner in this game
     */
     if ((table[0][0] === table[1][1] && table[1][1] === table[2][2] && table[0][0] === value_in_table) || 
         (table[0][2] === table[1][1] && table[1][1] === table[2][0] && table[0][2] === value_in_table)) {
-        showWinner(value_in_table)      
+        showWinner(value_in_table)
+
     } else if ((new Set(table[0]).size === 1) && table[0][0] === value_in_table) {
         showWinner(value_in_table)
     } else if ((new Set(table[1]).size === 1) && table[1][0] === value_in_table) {
@@ -88,24 +91,26 @@ function checkToWin(value_in_table = USER_TABLE_VALUE) {
 
 /* ********************** */
 
+document.oncontextmenu = () => {return false}
 document.getElementById("btn-clear").addEventListener("click", () => location.reload())
-// document.getElementById("btn-clear").addEventListener("click", clearTheTable)
 
 for (let row = 0; row < 3; row++) {
     for (let column = 0; column < 3; column++) {
         document.getElementById(`cell-${row}-${column}`).addEventListener("click", () => {
-            makeMove(row, column, USER_SYMBOL)
+            makeMove(row, column)
         })
     }
 }
 
-for (let row = 0; row < 3; row++) {
-    for (let column = 0; column < 3; column++) {
-        document.getElementById(`cell-${row}-${column}`).addEventListener("contextmenu", () => {
-            makeMove(row, column, ENEMY_SYMBOL)
-        })
-    }
-}
-
-document.getElementById("gamefield").addEventListener("click", checkToWin)
 /* Game logic */
+
+document.getElementById("gamefield").addEventListener("click", () => {
+    /* Analyze to make automaticly move on field */
+
+    // auto changing symbol ob label "next symbol is"
+    if (counterMoves % 2 === 0) {
+        document.getElementById("next-symbol").innerText = USER_SYMBOL
+    } else {
+        document.getElementById("next-symbol").innerText = ENEMY_SYMBOL
+    }
+})
